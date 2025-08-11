7543976fe295cf1bebdcb2b4000965ba852ee79c
koleesun0524 <koleesun0524@gmail.com>
2025-08-11 14:57:09 +0900

    PWA: add manifest & sw.js

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "khla-cache-v1";
+const CORE_ASSETS = ["/", "/index.html", "/manifest.webmanifest", "/icons/icon.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((c) => c.addAll(CORE_ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) =>
+      cached ||
+      fetch(event.request).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((c) => c.put(event.request, copy));
+        return res;
+      }).catch(() => caches.match("/"))
+    )
+  );
+});
